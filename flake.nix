9a1a9180ef4328c187ce1603a63f6ac07353d4ca
KongThePirate <KongThePirate@github.com>
2023-11-04 13:40:49 +0000

    add files'

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs = {
+      url = github:nixos/nixpkgs?ref=nixos-23.05;
+    };
+  };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = {
+    self,
+    nixpkgs, 
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  in {
+    packages.${system} = {
+	myPackage = pkgs.${system}.callPackage;
+	default = self.packages.myPackage;
+    };
+
 
   };
 }
